2cfb3a6b4114452c6e1d0b1c040bf9863e3a5d3d
ANIMESH MANDAL <animeshmandal4400@gmail.com>
2025-09-20 10:40:16 +0530

    folder structure created and ui added

--- app/src/main/java/com/animeshmandal/recipesearchapp/MainActivity.kt
+++ app/src/main/java/com/animeshmandal/recipesearchapp/MainActivity.kt
@@ -1,47 +1,39 @@
-package com.animeshmandal.recipesearchapp
+package com.animeshmandal.recipesearchapp.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.animeshmandal.recipesearchapp.ui.theme.RecipeSearchAppTheme
+import androidx.navigation.compose.rememberNavController
+import com.animeshmandal.recipesearchapp.presentation.navigation.RecipeNavigation
+import com.animeshmandal.recipesearchapp.presentation.theme.RecipeSearchAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RecipeSearchAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                RecipeSearchApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecipeSearchAppTheme {
-        Greeting("Android")
+fun RecipeSearchApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        val navController = rememberNavController()
+        RecipeNavigation(navController = navController)
     }
 }
